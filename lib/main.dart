34851e53cf76da4891e669c56e06b734e5c8a9d4
Mohammed Sajad VT <mohammedsajadvt@gmail.com>
2025-02-05 23:16:15 +0530

    Fixed Conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,23 @@
-<<<<<<< HEAD
 import 'package:inovest/business_logics/auth/auth_bloc.dart';
 import 'package:inovest/business_logics/checkbox/check_box_bloc.dart';
-import 'package:inovest/core/theme/theme.dart';
 import 'package:inovest/core/utils/app_routes.dart';
 import 'package:inovest/data/services/auth_service.dart';
 import '/core/utils/index.dart';
 
-void main()  {
+void main() {
   final AuthService authService = AuthService();
-=======
-// import 'package:firebase_core/firebase_core.dart';
-import 'package:inovest/business_logics/checkbox/check_box_bloc.dart';
-import 'package:inovest/core/utils/app_routes.dart';
-// import 'package:inovest/firebase_options.dart';
-import '/core/utils/index.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
->>>>>>> 30ca87d6817116739782f7d1759198a2e6408e42
   runApp(
-    MultiBlocProvider(providers: [
-
-      BlocProvider(
-        create: (context) => CheckBoxBloc(),
-      ),
-      BlocProvider(
-        create: (context) => AuthBloc(authService: authService),
-      ),
-    ], child: MyApp()),
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => CheckBoxBloc(),
+        ),
+        BlocProvider(
+          create: (context) => AuthBloc(authService: authService),
+        ),
+      ],
+      child: MyApp(),
+    ),
   );
 }
 
@@ -44,26 +31,12 @@ class MyApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
-<<<<<<< Updated upstream
-        return BlocBuilder<ThemeBloc, ThemeState>(
-          builder: (context, state) {
-            return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: 'Flutter Demo',
-              theme: state.themeData,
-              home: child,
-              initialRoute: AppRoutes.chats,
-              routes: AppRoutes.routes,
-            );
-          },
-=======
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Flutter Demo',
           home: child,
-          initialRoute: AppRoutes.entrepreneurHome,
+          initialRoute: AppRoutes.login,
           routes: AppRoutes.routes,
->>>>>>> Stashed changes
         );
       },
     );
